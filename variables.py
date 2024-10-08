0b569503900d6f5321a447305c6ed7778bfd3ac8
seraphincess <166063014+seraphincess@users.noreply.github.com>
2024-10-08 22:06:25 +0400

    переменные

---
+++ variables.py
@@ -0,0 +1,5 @@
+completed_tasks = 12
+hours = 1.5
+course = 'Python'
+time_for_task = hours / completed_tasks
+print('Курс:', course, ', всего задач:', completed_tasks, ', затрачено часов:', hours, 'среднее время выполнения:', time_for_task)
