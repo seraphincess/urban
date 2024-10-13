61d5012a89850fda1c87e088637b37ff88db9db0
seraphincess <166063014+seraphincess@users.noreply.github.com>
2024-10-13 22:09:44 +0400

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = (0, 'window', True)
+print(immutable_var)
+
+#immutable_var[0] = 2
+#кортежи - неизменяемый контейнер
+
+mutable_list = ([0, 1], 2, 3)
+mutable_list[0][0] = 4
+print(mutable_list)
