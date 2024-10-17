8a07ba4bb1a37a7f69c7a08dc3d899566ab37e3c
seraphincess <166063014+seraphincess@users.noreply.github.com>
2024-10-17 15:13:08 +0400

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -4,6 +4,6 @@ print(immutable_var)
 #immutable_var[0] = 2
 #кортежи - неизменяемый контейнер
 
-mutable_list = ([0, 1], 2, 3)
+mutable_list = [[0, 1], 2, 3]
 mutable_list[0][0] = 4
 print(mutable_list)
