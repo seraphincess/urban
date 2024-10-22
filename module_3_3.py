b7ac13a8d3764e368d5f08145a786d7193b105d9
seraphincess <166063014+seraphincess@users.noreply.github.com>
2024-10-22 16:10:57 +0400

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,14 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+print_params()
+#print_params(b = 25)
+#print_params(c = [1,2,3])
+
+values_list = [1, 'string', True]
+values_dict = {'a':2, 'b':'cat' ,'c':False}
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [3, False]
+print_params(*values_list_2, 42)
