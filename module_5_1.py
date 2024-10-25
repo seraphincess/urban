757f2facfccea48708f853c30652c84297f76ad0
seraphincess <166063014+seraphincess@users.noreply.github.com>
2024-10-25 18:58:05 +0400

    Update module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -3,12 +3,6 @@ class House:
         self.name = name
         self.number_of_floors = number_of_floors
 
-    def __len__(self):
-        return self.number_of_floors
-
-    def __str__(self):
-        return  f"Название: {str(self.name)}, кол-во этажей: {str(self.number_of_floors)}"
-
     def go_to(self, new_floor):
         if 1 <= new_floor < self.number_of_floors:
             for i in range(1, new_floor + 1):
@@ -17,12 +11,10 @@ class House:
                 print('Такого этажа не существует!')
 
 
-h1 = House('ЖК Эльбрус', 10)
-h2 = House('ЖК Акация', 20)
-
-# __str__
-print(h1)
-print(h2)
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
 
 # __len__
 print(len(h1))
